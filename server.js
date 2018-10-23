5c270ce196ece682f0606c7708463b37148e943a
Darrin Howell <dbhowell09@gmail.com>
2018-10-23 11:45:12 -0700

    successfully created getter to find lat and long

--- server.js
+++ server.js
@@ -12,21 +12,24 @@ const app = _express();
 app.use(_cors());
 
 app.get('/location', (request,response) => {
-    const locationData = searchToLatLong(request.query.data);
-    response.send(locationData);
-  });
+  const locationData = searchToLatLong(request.query.data);
+  response.send(locationData);
+  response.send('hello');
+});
 
 
-  function searchToLatLong(query) {
-    // Go to google (tomorrow)
-    const googleData = require(GOOGLE_MAPS);
-    const location = new Location(googleData.results[0]);
-      location.search_query = query;
-    return location;
-  }
+function searchToLatLong(query) {
+  // Go to google (tomorrow)
+  const googleData = require('./data/geo.json');
+  const location = new Location(googleData.results[0]);
+  location.search_query = query;
+  return location;
+}
 
-  function Location(data) { 
-    this.formatted_query = data.formatted_address;
-    this.latitude = data.geometry.location.lat;
-    this.longitude = data.geometry.location.lng;
-  }
+function Location(data) {
+//   this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+}
+
+app.listen(PORT, () => console.log(`App is up on ${PORT}`) );
